e88f619dc3fb8402efa28898ad96ba13e0e01718
Bufu <49696158+xbufu@users.noreply.github.com>
2021-02-12 11:09:12 +0100

    Update git_update.sh

--- git_update.sh
+++ git_update.sh
@@ -2,4 +2,4 @@
 
 cd /opt
 find . -type d -maxdepth 1 -exec git --git-dir={}/.git --work-tree=$PWD/{} pull \; | tee -a ~/.git_update/log.txt
-got get -u github.com/ffuf/ffuf
+go get -u github.com/ffuf/ffuf
