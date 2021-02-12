d2bed811786c0802cb754a099cb19d9576e5fb1a
Bufu <49696158+xbufu@users.noreply.github.com>
2021-02-12 10:58:00 +0100

    Update kali_setup.sh

--- kali_setup.sh
+++ kali_setup.sh
@@ -51,7 +51,7 @@ java_setup() {
 	# Set up Java
 	sudo apt install -y openjdk-11-jdk openjdk-11-jre openjdk-11-dbg openjdk-11-doc
 	echo -e "\n# Java\nexport JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64" >> ~/.bashrc
-	echo -e "\nexport PATH=\$PATH:\$JAVA_HOME/bin" >> ~/.bashrc
+	echo -e "export PATH=\$PATH:\$JAVA_HOME/bin" >> ~/.bashrc
 }
 
 go_setup() {
@@ -59,7 +59,7 @@ go_setup() {
 	wget https://golang.org/dl/go1.15.8.linux-amd64.tar.gz -O go1.15.8.linux-amd64.tar.gz
 	sudo tar -C /usr/local -xzf go1.15.8.linux-amd64.tar.gz
 	rm -f go1.15.8.linux-amd64.tar.gz
-	printf "\n\n# GoLang\nexport PATH=\$PATH:/usr/local/go/bin"
+	echo -e "\n# GoLang\nexport PATH=\$PATH:/usr/local/go/bin" >> ~/.bashrc
 }
 
 config_setup() {
