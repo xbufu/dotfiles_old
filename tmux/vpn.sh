6abea0e43db700eefd645c06f5ce2acb0cd139c7
xbufu <xbufu@pm.me>
2021-03-27 09:34:07 +0100

    updated tmux conf

--- tmux/vpn.sh
+++ tmux/vpn.sh
@@ -2,4 +2,6 @@
 
 if [ -d /proc/sys/net/ipv4/conf/tun0 ]; then
         echo "tun0 $({ ip -4 -br a sh dev tun0 | awk {'print $3'} | cut -f1 -d/; } 2>/dev/null)"
+elif [ -d /proc/sys/net/ipv4/conf/tun0 ]; then
+        echo "tap0 $({ ip -4 -br a sh dev tap0 | awk {'print $3'} | cut -f1 -d/; } 2>/dev/null)"
 fi
